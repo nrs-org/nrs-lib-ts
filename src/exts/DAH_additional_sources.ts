03e65fd73cc7af6cb30310f0d4d06c327f116f4e
ngoduyanh <ngoduyanh.chip@gmail.com>
2023-08-19 12:45:20 +0700

    feat(impl): :sparkles: impl extension

--- src/exts/DAH_additional_sources.ts
+++ src/exts/DAH_additional_sources.ts
@@ -1,11 +1,44 @@
+import { HasMeta, Meta } from "../data.ts";
+
 export class DAH_additional_sources {
     constructor(_: ExtConfig_DAH_additional_sources) {}
-    
+
     dependencies(): string[] {
         return [];
     }
-
-    
 }
 
-export type ExtConfig_DAH_additional_sources = Record<string | number | symbol, never>;
+export type ExtConfig_DAH_additional_sources = Record<
+    string | number | symbol,
+    never
+>;
+
+export interface AdditionalSources {
+    id_MyAnimeList?: number;
+    id_AniList?: number;
+    id_Kitsu?: number;
+    id_AniDB?: number;
+    id_VNDB?: number;
+    vgmdb?: VGMDBSource;
+    urls: URLSource[];
+}
+
+export interface VGMDBSource {
+    artist?: number;
+    album?: number;
+    disc?: number;
+    track?: number;
+    product?: number;
+}
+
+export interface URLSource extends HasMeta<Meta> {
+    name: string;
+    src: string;
+}
+
+declare module "../data.ts" {
+    interface EntryMeta {
+        // only present in entry meta
+        DAH_additional_sources?: AdditionalSources;
+    }
+}
