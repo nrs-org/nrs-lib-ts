c472268dd7814b1163cf977862c63f7d290dfa95
btmxh <ngoduyanh.chip@gmail.com>
2023-08-19 12:31:37 +0700

    feat: :sparkles: (wip) impl `DAH_additional_sources`

---
+++ src/exts/DAH_additional_sources.ts
@@ -0,0 +1,11 @@
+export class DAH_additional_sources {
+    constructor(_: ExtConfig_DAH_additional_sources) {}
+    
+    dependencies(): string[] {
+        return [];
+    }
+
+    
+}
+
+export type ExtConfig_DAH_additional_sources = Record<string | number | symbol, never>;
