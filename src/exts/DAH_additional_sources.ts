01c8c033d43ff76afe5dd4144485afaf761c116b
btmxh <ngoduyanh.chip@gmail.com>
2023-09-13 20:20:42 +0700

    feat: :sparkles: yt spotify altsrcs

--- src/exts/DAH_additional_sources.ts
+++ src/exts/DAH_additional_sources.ts
@@ -1,4 +1,5 @@
 import { HasMeta, Meta } from "../data.ts";
+import { Duration } from "../deps.ts";
 
 export class DAH_additional_sources {
     constructor(_: ExtConfig_DAH_additional_sources) {}
@@ -20,6 +21,8 @@ export interface AdditionalSources {
     id_AniDB?: number;
     id_VNDB?: number;
     vgmdb?: VGMDBSource;
+    youtube?: YoutubeSource;
+    spotify?: SpotifySource;
     urls?: URLSource[];
 }
 
@@ -35,6 +38,41 @@ export interface URLSource extends HasMeta<Meta> {
     name: string;
     src: string;
 }
+
+export type YoutubeSource =
+    | YoutubeVideoSource
+    | YoutubePlaylistSource
+    | YoutubeUserSource;
+
+export interface YoutubeVideoSource extends HasMeta<Meta> {
+    video: string;
+    from?: Duration;
+    to?: Duration;
+}
+
+export interface YoutubePlaylistSource extends HasMeta<Meta> {
+    playlist: string;
+}
+
+export type YoutubeUserSource =
+    | {
+          channelId: string;
+          channelHandle?: string;
+      }
+    | {
+          channelHandle: string;
+      };
+
+export type SpotifySource =
+    | {
+          track: string;
+      }
+    | {
+          album: string;
+      }
+    | {
+          artist: string;
+      };
 
 declare module "../data.ts" {
     interface EntryMeta {
